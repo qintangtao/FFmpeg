ad7ca6a964e0fbc0324190d96739d9923c342a84
Tangtao Qin <qintangtao@qq.com>
2023-04-10 17:02:10 +0800

    enable-libvpl replace enable-libmfx

---
+++ build_x86_x64_nv10.0_qsv_vpl.sh
@@ -0,0 +1,31 @@
+#https://blog.csdn.net/u012117034/article/details/123131144
+
+export PKG_CONFIG_PATH=/i/github/FFmpeg/third/libx264-x64/lib/pkgconfig:/i/github/FFmpeg/third/nv-sdk-v10.0/lib/pkgconfig:/i/github/FFmpeg/third/libvpl-msvc-x64/lib/pkgconfig:$PKG_CONFIG_PATH
+echo $PKG_CONFIG_PATH
+
+./configure --prefix=/f/ff/usr/ffmpeg-6.0/x86_x64/nv-10.0-qsv-vpl \
+			--enable-shared \
+			--disable-static \
+    		--enable-gpl \
+    		--enable-libx264 \
+			--enable-cuda-nvcc \
+			--enable-libvpl \
+			--enable-libnpp \
+			--enable-nonfree \
+			--toolchain=msvc \
+			--extra-cflags="-I/i/github/FFmpeg/third/libx264-x64/include -I/i/github/FFmpeg/third/nv-sdk-v10.0/include/cuda" \
+		    --extra-ldflags="-LIBPATH:/i/github/FFmpeg/third/libx264-x64/lib -LIBPATH:/i/github/FFmpeg/third/nv-sdk-v10.0/lib/cuda/x64"
+
+
+# oneVPL编译参考如下链接
+# https://github.com/oneapi-src/oneVPL.git
+
+
+# make 前需要把config.h文件中的中文删除掉
+# 修改前
+# #define CC_IDENT "用于 x64 的 Microsoft (R) C/C++ 优化编译器 19.16.27031.1 版"
+# 修改后
+# #define CC_IDENT ""
+
+#make -j8
+#make install
