dca4d18afa8c5a5fb5f1274b63a92be2939c0224
Tangtao Qin <qintangtao@qq.com>
2023-04-12 10:48:07 +0800

    add param width and height

--- doc/examples/encode_video_nv12.c
+++ doc/examples/encode_video_nv12.c
@@ -37,13 +37,9 @@
 #include <libavutil/imgutils.h>
 
 static void encode(AVCodecContext *enc_ctx, AVFrame *frame, AVPacket *pkt,
-                   FILE *outfile)
+                   FILE *fout)
 {
     int ret;
-
-    /* send the frame to the encoder */
-    if (frame)
-        printf("Send frame %3"PRId64"\n", frame->pts);
 
     ret = avcodec_send_frame(enc_ctx, frame);
     if (ret < 0) {
@@ -60,35 +56,42 @@ static void encode(AVCodecContext *enc_ctx, AVFrame *frame, AVPacket *pkt,
             exit(1);
         }
 
-        printf("Write packet %3"PRId64" (size=%5d)\n", pkt->pts, pkt->size);
-        fwrite(pkt->data, 1, pkt->size, outfile);
+        fwrite(pkt->data, 1, pkt->size, fout);
         av_packet_unref(pkt);
     }
 }
 
 int main(int argc, char **argv)
 {
-    const char *filename_in, *filename, *codec_name;
     const AVCodec *codec;
     AVCodecContext *c= NULL;
-    int i, ret, size, err;
-    FILE *fin, *f;
+    int i, ret, err, width, height, size;
+    FILE *fin, *fout;
     AVFrame *frame;
     AVPacket *pkt;
-    uint8_t endcode[] = { 0, 0, 1, 0xb7 };
 
-    if (argc <= 3) {
-        fprintf(stderr, "Usage: %s <input file> <output file> <codec name>\n", argv[0]);
+    if (argc <= 5) {
+        fprintf(stderr, "Usage: %s <width> <height> <input file> <output file> <codec name>\n", argv[0]);
         exit(0);
     }
-    filename_in = argv[1];
-    filename = argv[2];
-    codec_name = argv[3];
+    width  = atoi(argv[1]);
+    height = atoi(argv[2]);
+    size   = width * height;
+
+
+    if (!(fin = fopen(argv[3], "rb"))) {
+        fprintf(stderr, "Fail to open input file : %s\n", strerror(errno));
+        exit(1);
+    }
+    if (!(fout = fopen(argv[4], "w+b"))) {
+        fprintf(stderr, "Fail to open output file : %s\n", strerror(errno));
+        exit(1);
+    }
 
     /* find the mpeg1video encoder */
-    codec = avcodec_find_encoder_by_name(codec_name);
+    codec = avcodec_find_encoder_by_name(argv[5]);
     if (!codec) {
-        fprintf(stderr, "Codec '%s' not found\n", codec_name);
+        fprintf(stderr, "Codec '%s' not found\n", argv[5]);
         exit(1);
     }
 
@@ -105,8 +108,8 @@ int main(int argc, char **argv)
     /* put sample parameters */
     c->bit_rate = 400000;
     /* resolution must be a multiple of two */
-    c->width = 1920;
-    c->height = 1080;
+    c->width = width;
+    c->height = height;
     /* frames per second */
     c->time_base = (AVRational){1, 25};
     c->framerate = (AVRational){25, 1};
@@ -119,7 +122,6 @@ int main(int argc, char **argv)
      */
     c->gop_size = 10;
     c->max_b_frames = 1;
-    //c->pix_fmt = AV_PIX_FMT_YUV420P;
     c->pix_fmt = AV_PIX_FMT_NV12;
 
     if (codec->pix_fmts) {
@@ -132,9 +134,6 @@ int main(int argc, char **argv)
         }
     }
 
-    if (codec->id == AV_CODEC_ID_H264)
-        av_opt_set(c->priv_data, "preset", "slow", 0);
-
     /* open it */
     ret = avcodec_open2(c, codec, NULL);
     if (ret < 0) {
@@ -142,15 +141,6 @@ int main(int argc, char **argv)
         exit(1);
     }
 
-	if (!(fin = fopen(filename_in, "rb"))) {
-        fprintf(stderr, "Fail to open input file : %s\n", strerror(errno));
-        exit(1);
-    }
-    f = fopen(filename, "wb");
-    if (!f) {
-        fprintf(stderr, "Could not open %s\n", filename);
-        exit(1);
-    }
 
     frame = av_frame_alloc();
     if (!frame) {
@@ -166,15 +156,11 @@ int main(int argc, char **argv)
         fprintf(stderr, "Could not allocate the video frame data\n");
         exit(1);
     }
-
-	size = c->width * c->height;
     
     i = 0;
-
+    
     /* encode 1 second of video */
     while (1) {
-        fflush(stdout);
-
         /* Make sure the frame data is writable.
            On the first round, the frame is fresh from av_frame_get_buffer()
            and therefore we know it is writable.
@@ -189,7 +175,7 @@ int main(int argc, char **argv)
         if (ret < 0)
             exit(1);
 
-       if ((err = fread((uint8_t*)(frame->data[0]), size, 1, fin)) <= 0)
+        if ((err = fread((uint8_t*)(frame->data[0]), size, 1, fin)) <= 0)
             break;
         if ((err = fread((uint8_t*)(frame->data[1]), size/2, 1, fin)) <= 0)
             break;
@@ -197,21 +183,14 @@ int main(int argc, char **argv)
         frame->pts = i++;
 
         /* encode the image */
-        encode(c, frame, pkt, f);
+        encode(c, frame, pkt, fout);
     }
 
     /* flush the encoder */
-    encode(c, NULL, pkt, f);
+    encode(c, NULL, pkt, fout);
 
-    /* Add sequence end code to have a real MPEG file.
-       It makes only sense because this tiny examples writes packets
-       directly. This is called "elementary stream" and only works for some
-       codecs. To create a valid file, you usually need to write packets
-       into a proper file format or protocol; see muxing.c.
-     */
-    if (codec->id == AV_CODEC_ID_MPEG1VIDEO || codec->id == AV_CODEC_ID_MPEG2VIDEO)
-        fwrite(endcode, 1, sizeof(endcode), f);
-    fclose(f);
+   
+    fclose(fout);
     fclose(fin);
 
     avcodec_free_context(&c);
