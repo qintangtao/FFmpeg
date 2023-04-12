ce3c703b94d4ce8be476c40be0e5103bb1304abc
Tangtao Qin <qintangtao@qq.com>
2023-04-12 10:47:45 +0800

    update build.sh

--- doc/examples/build.sh
+++ doc/examples/build.sh
@@ -1,6 +1,16 @@
 #https://blog.csdn.net/u012117034/article/details/123131144
 
-export PKG_CONFIG_PATH=/f/ff/usr/ffmpeg-6.0/x86_x64/nv-10.0-qsv/lib/pkgconfig:$PKG_CONFIG_PATH
-echo $PKG_CONFIG_PATH
+FFMPEG_PATH=`pwd`/../..
+echo FFMPEG_PATH=$FFMPEG_PATH
+
+HOST_TAG=win
+ARCH=x86_x64
+VERSION_TAG=nv-10.1-qsv-vpl
+
+FFMPEG_LIB_PATH=$HOST_TAG/$ARCH/$VERSION_TAG
+echo FFMPEG_LIB_PATH=$FFMPEG_LIB_PATH
+
+export PKG_CONFIG_PATH=$FFMPEG_PATH/install/$FFMPEG_LIB_PATH/lib/pkgconfig:$PKG_CONFIG_PATH
+echo PKG_CONFIG_PATH=$PKG_CONFIG_PATH
 
 make -f Makefile.example.win
