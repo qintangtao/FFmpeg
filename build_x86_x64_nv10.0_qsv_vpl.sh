5b868280c3079b46d23ba860913d0ffe8689de13
Tangtao Qin <qintangtao@qq.com>
2023-04-19 11:49:13 +0800

    path

--- build_x86_x64_nv10.0_qsv_vpl.sh
+++ build_x86_x64_nv10.0_qsv_vpl.sh
@@ -1,9 +1,12 @@
 #https://blog.csdn.net/u012117034/article/details/123131144
 
-export PKG_CONFIG_PATH=/i/github/FFmpeg/third/libx264-x64/lib/pkgconfig:/i/github/FFmpeg/third/nv-sdk-v10.0/lib/pkgconfig:/i/github/FFmpeg/third/libvpl-msvc-x64/lib/pkgconfig:$PKG_CONFIG_PATH
+FFMPEG_PATH=`pwd`
+echo $FFMPEG_PATH
+
+export PKG_CONFIG_PATH=$FFMPEG_PATH/third/libx264-x64/lib/pkgconfig:$FFMPEG_PATH/third/nv-sdk-v10.0/lib/pkgconfig:$FFMPEG_PATH/third/libvpl-msvc-x64/lib/pkgconfig:$PKG_CONFIG_PATH
 echo $PKG_CONFIG_PATH
 
-./configure --prefix=/f/ff/usr/ffmpeg-6.0/x86_x64/nv-10.0-qsv-vpl \
+./configure --prefix=$FFMPEG_PATH/install/win/x86_x64/nv-10.0-qsv-vpl \
 			--enable-shared \
 			--disable-static \
     		--enable-gpl \
@@ -13,8 +16,8 @@ echo $PKG_CONFIG_PATH
 			--enable-libnpp \
 			--enable-nonfree \
 			--toolchain=msvc \
-			--extra-cflags="-I/i/github/FFmpeg/third/libx264-x64/include -I/i/github/FFmpeg/third/nv-sdk-v10.0/include/cuda" \
-		    --extra-ldflags="-LIBPATH:/i/github/FFmpeg/third/libx264-x64/lib -LIBPATH:/i/github/FFmpeg/third/nv-sdk-v10.0/lib/cuda/x64"
+			--extra-cflags="-I$FFMPEG_PATH/third/libx264-x64/include -I$FFMPEG_PATH/third/nv-sdk-v10.0/include/cuda" \
+		    --extra-ldflags="-LIBPATH:$FFMPEG_PATH/third/libx264-x64/lib -LIBPATH:$FFMPEG_PATH/third/nv-sdk-v10.0/lib/cuda/x64"
 
 
 # oneVPL编译参考如下链接
